459f136257d91077f914ae0872dd7d33af1fd544
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2025-09-11 16:16:13 -0300

    Add files via upload

---
+++ Revisão Prova/Revisao/src/Quarto.java
@@ -0,0 +1,59 @@
+public class Quarto {
+    private int numero;
+    private String tipo;
+    private double precoPorNoite;
+    private boolean ocupado;
+    private Hospede hospedeAtual;
+
+    public Quarto(int numero, String tipo, double precoPorNoite){
+        this.numero = numero;
+        this.tipo = tipo;
+        this.precoPorNoite = precoPorNoite;
+        this.ocupado = false;
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    public boolean isOcupado(){
+        return this.ocupado;
+    }
+
+    public Hospede getHospedeAtual(){
+        return this.hospedeAtual;
+    }
+
+    public double getprecoPorNoite(){
+        return this.precoPorNoite;
+    }
+
+    public void ocupar(Hospede hospede){
+        setOcupado(true);
+        setHospedeAtual(hospede);
+    }
+
+    public void liberar(){
+        setOcupado(false);
+        setHospedeAtual(null);
+    }
+
+    public void setHospedeAtual(Hospede hospede){
+        this.hospedeAtual = hospede;
+    }
+
+    public void setOcupado(boolean ocupado){
+        this.ocupado = true;
+    }
+
+
+
+
+
+
+
+}
